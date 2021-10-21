4a55d7fa8eb6e1e4bdcf9b025d525e4b70adb538
William Lucht <aaronlucht@gmail.com>
2021-10-20 19:45:06 -0700

    added poe api environment details. Added .env file. Brought in bootstrap 4 grid(may revert to Tailwind grid - still toying with both grid layout styles/functionality). Added content to About page component.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,9 +3,59 @@ import "./about.css";
 
 function About() {
   return (
-    <div className="flex items-center justify-center mt-3">
-      <h2 style={{ color: "white" }}>About the Developer</h2>
-    </div>
+    <>
+      <div className="container about-page p-8">
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-4">
+            <h2 className="justify-content-center text-center">
+              About the Developer
+            </h2>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-10 mt-2">
+            <p className="text-center">
+              Hi, my name is Emericas! I am an avid fan of Path of Exile and a
+              Web Developer based out of California. I have created
+              PoE-Paperdoll as a pet project that consumes the Path of Exile
+              API. If you like what you see and wish to contribute to this
+              project, please feel free to fork the project{" "}
+              <a
+                href="https://github.com/d606n6k/poe-paperdoll"
+                target="_blank"
+                rel="noreferrer"
+                className="text-red-400 hover:text-red-600"
+              >
+                here
+              </a>
+              .
+            </p>
+          </div>
+        </div>
+        {/* <div className="row justify-content-center">
+          <div className="col-12-auto">
+            <h2 className="justify-content-center">About the Developer</h2>
+          </div>
+          <div className="col-12-auto mt-2">
+            <p className="">
+              Hi, my name is Emericas! I am an avid fan of Path of Exile and a
+              Web Developer based out of California. I have created
+              PoE-Paperdoll as a pet project that consumes the Path of Exile
+              API. If you like what you see and wish to contribute to this
+              project, please feel free to fork the project{" "}
+              <a
+                href="https://github.com/d606n6k/poe-paperdoll"
+                target="_blank"
+                rel="noreferrer"
+                className="text-red-400 hover:text-red-600"
+              >
+                here
+              </a>
+            </p>
+          </div>
+        </div> */}
+      </div>
+    </>
   );
 }
 
