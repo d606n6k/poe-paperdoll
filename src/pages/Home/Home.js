67986c6388c9f6328f8dbfdd7e9320cc014f31f2
William Lucht <aaronlucht@gmail.com>
2022-03-27 14:22:26 -0700

    moved account search to the 'AccountSearch' componentas it is more syntactically correct for the app. Began work on homepage content. Added some text style for PoE link. Misc other layout cleanup

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -13,48 +13,13 @@ function Home() {
             </h2>
             <br />
           </div>
-          <div className="flex items-center justify-center">
-            <p className="">
-              Enter your account name below to bring up a list of your
-              characters. Note: Please make sure that your account is public
-              inside your{" "}
-              <a
-                href="https://www.pathofexile.com/my-account"
-                target="_blank"
-                rel="noreferrer"
-                className="text-red-400 hover:text-red-600"
-              >
-                Path of Exile Account
-              </a>
+          <div>
+            <p className="flex items-center justify-center">
+              Welcome to the beta version of PoE Paperdoll. The online character window for the video game <a href="https://www.pathofexile.com" target="_blank" className="text-red-400 hover:text-red-600">Path of Exile</a>.
             </p>
+            <br />
           </div>
         </div>
-        <div className="grid grid-cols-1 sm-grid-cols-4 mt-3 ">
-          <div className="flex items-center justify-center">
-            <input
-              type="text"
-              className="find-account-input w-1/3 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            ></input>
-          </div>
-          <div className="flex items-center justify-center">
-            <button className="find-account-button w-1/3 mt-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
-              Find Your Account
-            </button>
-          </div>
-        </div>
-        {/* <div className="flex items-center justify-center mt-3">
-        <div className="grid grid-cols-3 gap-4">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
-          <div>7</div>
-          <div>8</div>
-          <div>9</div>
-        </div>
-      </div> */}
       </div>
     </>
   );
