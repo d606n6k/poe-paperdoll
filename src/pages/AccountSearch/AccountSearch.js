85bcb37755b5354a8e329a9a9ccd813de7a94302
William Lucht <aaronlucht@gmail.com>
2022-10-16 11:27:14 -0700

    set up AccountSearch with useRef for account button click. Working on fetching from POE api.

--- src/pages/AccountSearch/AccountSearch.js
+++ src/pages/AccountSearch/AccountSearch.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useRef, useEffect, useState } from "react";
+import { useRef } from "react";
 import "./accountsearch.css";
 
 // define a variable for account name
@@ -19,22 +19,21 @@ import "./accountsearch.css";
 // }
 
 const AccountSearch = () => {
-  const [accountFetch, setAccount] = useState([]);
-  const ref = useRef(null)
-  useEffect(() => {
-    // findAccountBtn.on("click", getAccount());
-    const handleClick = event =>{
-      console.log('Button clicked')
-    }
-
-    const element = ref.current;
-
-    element.addEventListener('click', handleClick);
-
-    return () => {
-      element.removeEventListener("click", handleClick);
-    }
-  },[]);
+  const accountRef = useRef();
+  // TODO: capture account name and plug it into string for the fetch url
+  let poeFetchUrl = "https://www.pathofexile.com/character-window/get-characters?accountName=Emericas21";
+  const getAccount = (event) => {
+    event.preventDefault();
+    console.log({
+      account: accountRef.current.value,
+    });
+    // this is where I will want to fetch the data
+    fetch(poeFetchUrl).then((response) => {
+      return response.json();
+    }).then((data)=>{
+      console.log(data)
+    });
+  };
 
   return (
     <>
@@ -60,17 +59,20 @@ const AccountSearch = () => {
           </div>
         </div>
         <div className="grid grid-cols-1 sm-grid-cols-4 mt-3 ">
-          <div className="flex items-center justify-center">
-            <input
-              type="text"
-              className="find-account-input w-1/3 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            ></input>
-          </div>
-          <div className="flex items-center justify-center">
-            <button className="find-account-button w-1/3 mt-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" ref={ref}>
-              Find Your Account
-            </button>
-          </div>
+          <form onSubmit={getAccount}>
+            <div className="flex items-center justify-center">
+              <input
+                type="text"
+                className="find-account-input w-1/3 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                ref={accountRef}
+              ></input>
+            </div>
+            <div className="flex items-center justify-center">
+              <button className="find-account-button w-1/3 mt-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                Find Your Account
+              </button>
+            </div>
+          </form>
         </div>
         {/* <div className="flex items-center justify-center mt-3">
     <div className="grid grid-cols-3 gap-4">
